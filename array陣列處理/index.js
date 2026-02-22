1153ae84ec189f4e36ff7f8b2a2099e3ecbf1e8f
Sarah <sarah001160@gmail.com>
2026-02-22 22:31:18 +0800

    更新

--- array陣列處理/index.js
+++ array陣列處理/index.js
@@ -1,46 +1,125 @@
-const list = [
-    { name: 'banana', num: 10, id: 1 },
+const list = ['a', 'b', 'c']
+
+//push 新增到最後一個
+list.push(1)
+console.log(list) // [ 'a', 'b', 'c', 1 ]
+
+//pop刪除最後一個
+list.pop()
+console.log(list) // ['a', 'b', 'c']
+
+// unshift()新增到第一個
+list.unshift('new')
+console.log(list) //['new', 'a', 'b', 'c']
+
+//刪除第一個
+list.shift()
+console.log(list) // [ 'a', 'b', 'c' ]
+
+const index = list.indexOf('new')
+console.log(index) //-1
+
+const index2 = list.indexOf('b')
+console.log(index2) //1 (js從0開始序號，第二個元素的序號為1)
+
+//splice刪除指定
+list.splice(index2, 1)
+console.log(list) //[ 'a', 'c' ]
+
+//splice 新增的元素直接安排在指定索引位置。
+// array.splice(索引, 刪除數, 新增值)
+list.splice(0, 0, 'k')
+console.log(list) // [ 'k', 'a', 'c' ]
+
+list.splice(2, 0, 'k')
+console.log(list) // ['k', 'a', 'k', 'c']
+
+//指定索引的元素刪除、新增元素x
+list.splice(1, 1, 'x')
+console.log(list) // [ 'k', 'x', 'k', 'c' ]
+
+list.sort((a, b) => {
+    if (a < b) return -1
+    if (a > b) return 1
+    return 0
+})
+
+console.log(list) //c,k,k,x
+
+const test = [3, 2, 1, 6]
+test.sort((a, b) => {
+    if (a < b) return -1 //-1 把a排前面
+    if (a > b) return 1 // 1 把a排後面
+    return 0
+})
+console.log(test) //1,2,3,6
+
+//大-小
+test.sort((a, b) => {
+    if (a < b) return 1
+    if (a > b) return -1
+    return 0
+})
+console.log(test) //6321
+
+const list2 = [
     {
-        name: 'candy',
-        num: 20,
-        id: 2,
+        price: 100,
+        name: 'food',
     },
-    { name: 'uklele', num: 40, id: 3 },
+    {
+        price: 250,
+        name: 'drink',
+    },
+    { price: 6, name: 'candy' },
 ]
 
-//push 尾端新增元素
-list.push('1')
-console.log(list)
+const newList = list2.slice()
+console.log('n', newList)
 // [
-//   { name: 'banana', num: 10, id: 1 },
-//   { name: 'candy', num: 20, id: 2 },
-//   { name: 'uklele', num: 40, id: 3 },
-//   '1'
+//     ({ price: 100, name: 'food' },
+//     { price: 250, name: 'drink' },
+//     { price: 6, name: 'candy' })
 // ]
 
-//pop移除最後一個元素
-list.pop()
-console.log(list)
-// [
-//     ({ name: 'banana', num: 10, id: 1 },
-//     { name: 'candy', num: 20, id: 2 },
-//     { name: 'uklele', num: 40, id: 3 })
-// ]
+// 潛拷貝 第0筆開始 到第3筆為止(不含)的元素
+const newList2 = list2.slice(0, 2)
+console.log('n2', newList2) //[ { price: 100, name: 'food' }, { price: 250, name: 'drink' } ]
 
-// unshift()	從 前端 新增元素	array.unshift(0)
-list.unshift('0000')
-console.log(list)
-// [
-//     ('0000',
-//     { name: 'banana', num: 10, id: 1 },
-//     { name: 'candy', num: 20, id: 2 },
-//     { name: 'uklele', num: 40, id: 3 })
-// ]
+function sortF(type, key, arr) {
+    if (type == 'asc') {
+        const result = arr.slice().sort((a, b) => {
+            if (a[key] < b[key]) return -1
+            if (a[key] > b[key]) return 1
+            return 0
+        })
+        console.log(result)
+        return result
+    }
+    if (type == 'desc') {
+        const result = arr.sort((a, b) => {
+            if (a[key] < b[key]) return 1
+            if (a[key] > b[key]) return -1
+            return 0
+        })
+        console.log(result)
+        return result
+    }
+}
 
-list.shift()
-console.log(list)
-// shift()	移除 第一個 元素	array.shift()
-// splice()	增、刪、改 萬用工具	array.splice(索引, 刪除數, 新增值)
-// sort()	排序 (預設為 Unicode 排序)	array.sort((a, b) => a - b)
-// reverse()	反轉陣列順序	array.reverse()
+sortF('asc', 'price', list2)
+
+const list3 = [2, 3, 4]
+list3.reverse()
+console.log(list3) //4,3,2
+
 // fill()
+//array.fill(value, start, end)
+//你想建立一個長度為 5，且裡面全是 0 的陣列：
+const tempArr = new Array(5).fill(0)
+console.log(tempArr) //[0,0,0,0,0]
+
+//指定範圍都變成X
+//起始索引從 2 到結束索引 4 為止(不含結束索引)
+tempArr.fill('X', 2, 4)
+console.log(tempArr) //[(0, 0, 'X', 'X', 0)]
